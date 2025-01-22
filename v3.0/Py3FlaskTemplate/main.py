561e38fd8ae39c5f4682d6d75b9d71e31c168358
ScorpionInc <d2dejohn@gmail.com>
2025-01-22 12:17:52 -0500

    Update Py3FlaskTemplate
    Add favicon.ico. Changed framework template.

--- v3.0/Py3FlaskTemplate/main.py
+++ v3.0/Py3FlaskTemplate/main.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_from_directory
 
 from flask_wtf import FlaskForm
+
+import os
 
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+
+# Reference: https://flask.palletsprojects.com/en/stable/patterns/favicon/
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def serve_index():
